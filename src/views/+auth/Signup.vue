77231021b15e173ba2c0fa3062f2b96d59760bfb
Chiheb Nabil <chiheb.design@gmail.com>
2019-11-29 11:46:17 +0100

    signup page

---
+++ src/views/+auth/Signup.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="row d-flex justify-content-center">
+    <b-form @submit="onSubmit" class="col-md-6">
+      <b-form-group id="input-group-1" label="Username:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.username"
+          type="text"
+          required
+          placeholder="Enter username"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="password"
+          v-model="form.password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+  components: {},
+  data() {
+    return {
+      form: {
+        username: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/+auth/index.js
+++ src/views/+auth/index.js
@@ -1,10 +1,17 @@
 import Login from "./Login";
+import Signup from "./Signup";
 
 export default [
   {
     name: "auth.login",
     path: "/login",
     component: Login,
+    meta: { requiresAuth: false }
+  },
+  {
+    name: "auth.signup",
+    path: "/signup",
+    component: Signup,
     meta: { requiresAuth: false }
   }
 ];
